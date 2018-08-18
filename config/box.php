38e243418b2b9edd41d54dbd43f0ad01803aa25e
David Carr <dave@daveismyname.com>
2018-08-18 18:45:47 +0100

    updated config

--- config/box.php
+++ config/box.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'clientId'       => env('BOX_CLIENT_ID', 'w1fmmn8yd6lbx8y956rsgrb2s01ul4w4'),
-    'clientSecret'   => env('BOX_SECRET_ID', 'aFZNWExesb2GsCQxWy2w03c3WtW2udKN'),
+    'clientId'       => env('BOX_CLIENT_ID'),
+    'clientSecret'   => env('BOX_SECRET_ID'),
     'redirectUri'    => url('box/oauth'),
     'boxLandingUri'  => url('box'),
     'urlAuthorize'   => 'https://account.box.com/api/oauth2/authorize',
