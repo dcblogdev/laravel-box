da1e40a2ccd944a46d0e88d12ed56c5c7ab1367f
David Carr <dave@daveismyname.com>
2018-08-19 20:36:09 +0100

    adjusted migrations path

--- src/BoxServiceProvider.php
+++ src/BoxServiceProvider.php
@@ -18,6 +18,12 @@ class BoxServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/routes.php');
 
+        $timestamp = date('Y_m_d_His', time());
+
+        $this->publishes([
+            __DIR__.'/database/migrations/create_box_tokens_tables.php' => $this->app->databasePath()."/migrations/{$timestamp}_create_box_tokens_tables.php",
+        ], 'migrations');
+
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
 
