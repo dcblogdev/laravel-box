f047504235e3fc7e74185a86125b68f101276aa0
David Carr <dave@daveismyname.com>
2018-08-19 23:35:27 +0100

    changed config to accept paths for box routes

--- config/box.php
+++ config/box.php
@@ -3,8 +3,8 @@
 return [
     'clientId'       => env('BOX_CLIENT_ID'),
     'clientSecret'   => env('BOX_SECRET_ID'),
-    'redirectUri'    => url('box/oauth'),
-    'boxLandingUri'  => url('box'),
+    'redirectUri'    => env('BOX_REDIRECT_URI', url('box/oauth')),
+    'boxLandingUri'  => env('BOX_LANDING_URI', url('box')),
     'urlAuthorize'   => 'https://account.box.com/api/oauth2/authorize',
     'urlAccessToken' => 'https://www.box.com/api/oauth2/token',
 ];
