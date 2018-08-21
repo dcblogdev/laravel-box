e6ba539078106a61cd940520e4036c2b288038cc
David Carr <dave@daveismyname.com>
2018-08-21 18:47:28 +0100

    removed url from config

--- config/box.php
+++ config/box.php
@@ -3,8 +3,8 @@
 return [
     'clientId'       => env('BOX_CLIENT_ID'),
     'clientSecret'   => env('BOX_SECRET_ID'),
-    'redirectUri'    => env('BOX_REDIRECT_URI', url('box/oauth')),
-    'boxLandingUri'  => env('BOX_LANDING_URI', url('box')),
+    'redirectUri'    => env('BOX_REDIRECT_URI'),
+    'boxLandingUri'  => env('BOX_LANDING_URI'),
     'urlAuthorize'   => 'https://account.box.com/api/oauth2/authorize',
     'urlAccessToken' => 'https://www.box.com/api/oauth2/token',
 ];
